50aec8bdae84147ab822de346c61b3e1679c2d01
MerrickO <programmerscodes@gmail.com>
2023-02-20 20:03:13 +0200

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print out 'with proper grammer, but the outcome is a piece if art,'
+ *
+ * description - prints out specific set string
+ *
+ * return always 0
+ */
+
+int main(void)
+{
+	printf("%c","with proper grammer, but the outcome is a piece of art,")
+	return (0);
+}
