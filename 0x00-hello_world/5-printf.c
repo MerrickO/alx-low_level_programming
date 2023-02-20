70681182a25c133ab50f313e3dc2b9fbac4929c4
MerrickO <programmerscodes@gmail.com>
2023-02-20 20:08:45 +0200

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print out 'with proper grammer, but the outcome is a piece if art,'
- *
- * description - prints out specific set string
- *
- * return always 0
+ * main - always return 0
+ * description - print out 'with proper grammer, but the outcome is a piece if art,'
  */
 
 int main(void)
 {
-	printf("%s\n","with proper grammer, but the outcome is a piece of art,");
+	printf("%s\n","with proper grammer, but the outcome is a piece of art, ");
 	return (0);
 }
