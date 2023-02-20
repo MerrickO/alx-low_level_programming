2da0030d49a554d85e993d2d1120136c1e430475
MerrickO <programmerscodes@gmail.com>
2023-02-20 19:51:39 +0200

    update puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Main - entrypoint
+ *
+ * Description - prints out specific string
+ *
+ * Return always 0
+ */
+
 int main(void)
 {
-	char str[] = "Programming is like buliding a multilingual puzzle";
+	char str[] = "\"Programming is like buliding a multilingual puzzle";
 
 	puts(str);
 	return (0);
